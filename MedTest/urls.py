cd5380a9705aa965fd94fba5c4098a0d3dd349bd
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-03-30 16:32:33 +0100

    installed django and other packages

---
+++ MedTest/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = "Medical Test Scheduler "
+admin.site.site_title = "Medical Test Scheduler "
+admin.site.index_title = "Welcome to Medical Test Scheduler "
---
+++ MedTest_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedtestAuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "MedTest_auth"
