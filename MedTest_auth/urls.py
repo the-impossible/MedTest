39ca85ac9588c387affde5077bad4b0e9e56f4d4
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-04-01 04:24:35 +0100

    logout completed

--- MedTest_auth/urls.py
+++ MedTest_auth/urls.py
@@ -5,9 +5,11 @@ app_name = "auth"
 from MedTest_auth.views import  (
     DashboardView,
     LoginView,
+    LogoutView,
 )
 
 urlpatterns = [
     path('dashboard', DashboardView.as_view(), name="dashboard"),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
